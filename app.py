380033d214078a6939f478a0f69f3a42400879cd
Santiago Rivera Marin <138415229+Srhvh765@users.noreply.github.com>
2025-02-12 01:05:28 +0000

    Corregí los errores del local TRG

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from model.Person import Person
+import math
 
 print('SRM')
 print('TRG')
+print(Math.PI)
+
 trg = Person('Tutos', 19, 60, 179)
 print(trg)
-import math
-print('SRM')
-print('TRG')
-print(Math.PI)
