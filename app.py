abbb5c0bb1c56da2807f8bfe1bd132b805cc1d2a
tutosrivegamerlq <tutosrivegamer@gmail.com>
2025-02-12 20:47:09 -0500

    Añadi el modulo chromolog, TRG

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from model.Company import Company
 from model.Employee import Employee
+import chromolog as ch
+
+# Colour printer
+printer = ch.Print()
 
 # Companies
 my_company = Company('SRM-TRG', 'Empresa Unipersonal', 'Microempresa', 'Terciario', 'Privada')
@@ -9,5 +13,5 @@ srm = Employee('Santiago Marin', 19, 60, 180, 'Developer', 2500000, 'SRM')
 trg = Employee('Tutos Rive', 19, 60, 180, 'Developer', 2500000, 'SRM-TRG')
 
 # Imprimir información
-print(my_company.add_employees(srm, trg))
-print(my_company.get_all_employees_str())
+printer.suc(my_company.add_employees(srm, trg))
+printer.inf(my_company.get_all_employees_str())
