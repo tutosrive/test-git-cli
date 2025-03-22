a041607a3203c9f3353714b6f6e7d2b587d4297e
tutosrive <tutosrivegamer@gmail.com>
2025-03-21 22:05:32 -0500

    Add feature get_employee to Company object

--- app.py
+++ app.py
@@ -13,7 +13,12 @@ srm = Employee('Santiago Marin', 19, 60, 180, 'Developer', 2500000, 'SRM')
 trg = Employee('Tutos Rive', 19, 60, 180, 'Developer', 2500000, 'SRM-TRG')
 
 # Imprimir información
-printer.suc(my_company.add_employees(srm, trg))
-printer.inf(my_company.get_all_employees_str())
-printer.suc(my_company)
-printer.inf('Final de la ejecución...')
+#printer.suc(my_company.add_employees(srm, trg))
+#printer.inf(my_company.get_all_employees_str())
+#printer.suc(my_company)
+#printer.inf('Final de la ejecución...')
+
+
+
+my_company.add_employees(srm, trg)
+print(my_company.get_employee('Santiago Marin'))
