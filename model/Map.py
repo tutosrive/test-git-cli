8cf57b9e56f681d78720731889b5186bb8af4c7a
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-03-26 16:51:57 -0500

    Update Map.py

--- model/Map.py
+++ model/Map.py
@@ -2,6 +2,9 @@ class Map:
   def __init__(self):
     self.__site:str = 'Colombia'
     self.__size:int = 5
+
+ def getSize(self):
+	 return self.__size
     
 	def getSite(self) -> str:
 	  return self.__site
